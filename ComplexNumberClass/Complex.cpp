e94056adf9ce3cf464444d05a0abd883ef5dd5cb
eugenol <eug@live.co.za>
2015-03-07 12:55:47 +0200

    Flesh out Complex class

--- ComplexNumberClass/Complex.cpp
+++ ComplexNumberClass/Complex.cpp
@@ -3,9 +3,33 @@
 
 Complex::Complex()
 {
+	Im = 0;
+	Re = 0;
 }
 
 
 Complex::~Complex()
 {
+}
+
+void Complex::setReal(int real)
+{
+	Re = real;
+}
+void Complex::setImaginary(int imaginary)
+{
+	Im = imaginary;
+}
+int Complex::getReal(void)
+{
+	return Re;
+}
+int Complex::getImaginary()
+{
+	return Im;
+}
+void Complex::setComplex(int real, int imaginary)
+{
+	Re = real;
+	Im = imaginary;
 }
--- ComplexNumberClass/Complex.h
+++ ComplexNumberClass/Complex.h
@@ -2,9 +2,17 @@
 #define COMPLEX_H
 class Complex
 {
+	int Re;
+	int Im;
 public:
 	Complex();
 	~Complex();
+
+	void setReal(int real);
+	void setImaginary(int imaginary);
+	int getReal(void);
+	int getImaginary();
+	void setComplex(int real, int imaginary);
 };
 #endif
 
